ba1a8280aaf25e418e3a9741c338bf500f4e407f
jaebungs <jaebungs@gmail.com>
2021-03-11 16:26:28 -0500

    update contact icon click opens new tab. learned rel=noreferrer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Contact = () => {
   return (
     <section className="contact flex--column">
-      <a href="https://github.com/jaebungs" className="contact__anchor">
+      <a href="https://github.com/jaebungs" target="_blank" rel="noopener noreferrer" className="contact__anchor">
         <i className="fab fa-github-square contact__icon"></i>
       </a>
-      <a href="https://www.linkedin.com/in/jae-park-robotics" className="contact__anchor">
+      <a href="https://www.linkedin.com/in/jae-park-robotics" target="_blank" rel="noopener noreferrer" className="contact__anchor">
         <i className="fab fa-linkedin contact__icon"></i>
       </a>
-      <a className="contact__anchor" href="mailto:jaebungs@gmail.com"><i className="fas fa-at contact__icon"></i></a>
+      <a className="contact__anchor" href="mailto:jaebungs@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fas fa-at contact__icon"></i></a>
     </section>
   );
 };
