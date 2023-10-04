17876aa2e88fab8aabd86ebb369e83443022f84f
jaebungs <jaebungs@gmail.com>
2023-10-04 17:07:40 -0400

    fix heights

--- src/App.js
+++ src/App.js
@@ -16,32 +16,32 @@ function App() {
 
   return (
     <main className="app">
-      <Parallax className="about-section-parallax" pages={6.8}>
+      <Parallax className="about-section-parallax" pages={8}>
         <ParallaxLayer className="contact-parallax" offset={0.05} factor={0.1} speed={-1}>
           <Contact />
         </ParallaxLayer>
         <ParallaxLayer offset={0.1} factor={0} speed={0.1}>
           <About />
         </ParallaxLayer>
-        <ParallaxLayer offset={1} factor={0} speed={0.1}>
+        <ParallaxLayer offset={1.3} factor={0} speed={0.1}>
           <CT />
         </ParallaxLayer>
-        <ParallaxLayer offset={2} factor={0} speed={0.1}>
+        <ParallaxLayer offset={2.4} factor={0} speed={0.1}>
           <TCO />
         </ParallaxLayer>    
-        <ParallaxLayer offset={2.9} factor={0} speed={0.1}>
+        <ParallaxLayer offset={3.5} factor={0} speed={0.1}>
           <FluidTypo />
         </ParallaxLayer>
-        <ParallaxLayer offset={3.9} factor={0} speed={0.1}>
+        <ParallaxLayer offset={4.7} factor={0} speed={0.1}>
           <Visualizer />
         </ParallaxLayer>   
-        <ParallaxLayer offset={4.9} factor={0} speed={0.1}>
+        <ParallaxLayer offset={5.9} factor={0} speed={0.1}>
           <Cocktail />
         </ParallaxLayer>
-        <ParallaxLayer offset={6} factor={0} speed={0.1}>
+        <ParallaxLayer offset={7} factor={0} speed={0.1}>
           <Masonry />
         </ParallaxLayer>
-        <ParallaxLayer offset={6.6} speed={0.1}>
+        <ParallaxLayer offset={7.7} speed={0.1}>
           <Footer />
         </ParallaxLayer>
       </Parallax>
